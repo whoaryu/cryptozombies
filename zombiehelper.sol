fe2ea662fd07e662002929ac0a3794ae630a4958
whoaryu <105057945+whoaryu@users.noreply.github.com>
2024-04-03 11:00:14 +0530

    Update zombiehelper.sol

--- zombiehelper.sol
+++ zombiehelper.sol
@@ -4,9 +4,20 @@ import "./zombiefeeding.sol";
 
 contract ZombieHelper is ZombieFeeding {
 
+  uint levelUpFee = 0.001 ether;
+
   modifier aboveLevel(uint _level, uint _zombieId) {
     require(zombies[_zombieId].level >= _level);
     _;
+  }
+
+  // 1. Create withdraw function here
+
+  // 2. Create setLevelUpFee function here
+
+  function levelUp(uint _zombieId) external payable {
+    require(msg.value == levelUpFee);
+    zombies[_zombieId].level++;
   }
 
   function changeName(uint _zombieId, string calldata _newName) external aboveLevel(2, _zombieId) {
@@ -20,7 +31,15 @@ contract ZombieHelper is ZombieFeeding {
   }
 
   function getZombiesByOwner(address _owner) external view returns(uint[] memory) {
-    
+    uint[] memory result = new uint[](ownerZombieCount[_owner]);
+    uint counter = 0;
+    for (uint i = 0; i < zombies.length; i++) {
+      if (zombieToOwner[i] == _owner) {
+        result[counter] = i;
+        counter++;
+      }
+    }
+    return result;
   }
 
 }
